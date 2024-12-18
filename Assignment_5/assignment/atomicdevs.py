f3054497ac9f59ac365d3e038a12f8531ad32295
Jason Liu <liujason2003@gmail.com>
2024-12-18 12:19:39 +0100

    State for every atomic dev created

--- Assignment_5/assignment/atomicdevs.py
+++ Assignment_5/assignment/atomicdevs.py
@@ -5,11 +5,21 @@ from pypdevs.DEVS import AtomicDEVS
 from environment import *
 import random
 import dataclasses
+import queue
+
+@dataclasses.dataclass
+class QueueState:
+    queues: dict[int, queue.Queue]
+    
+    def __init__(self, ship_sizes):
+        self.queues = {size: queue.Queue() for size in ship_sizes}
 
 class Queue(AtomicDEVS):
     def __init__(self, ship_sizes):
         super().__init__("Queue")
-        # self.state = QueueState(...)
+        
+        self.ship_sizes = ship_sizes
+        self.state = QueueState(ship_sizes=ship_sizes)
 
     # def extTransition(self, inputs):
     #     pass
@@ -26,13 +36,24 @@ class Queue(AtomicDEVS):
 PRIORITIZE_BIGGER_SHIPS = 0
 PRIORITIZE_SMALLER_SHIPS = 1
 
+@dataclasses.dataclass
+class LoadBalancerState:
+    lock_capacities: list[int] 
+    current_lock: int  # Index of the lock for round-robin strategy
+    priority: int  # Priority type (e.g., PRIORITIZE_BIGGER_SHIPS or PRIORITIZE_SMALLER_SHIPS)
+
+    def __init__(self, lock_capacities, priority):
+        self.lock_capacities = lock_capacities[:]
+        self.current_lock = 0
+        self.priority = priority
+
 class RoundRobinLoadBalancer(AtomicDEVS):
     def __init__(self,
         lock_capacities=[3,2], # two locks of capacities 3 and 2.
         priority=PRIORITIZE_BIGGER_SHIPS,
     ):
         super().__init__("RoundRobinLoadBalancer")
-        # self.state = LoadBalancerState(...)
+        self.state = LoadBalancerState(lock_capacities=lock_capacities, priority=priority)
 
     # def extTransition(self, inputs):
     #     pass
@@ -52,7 +73,7 @@ class FillErUpLoadBalancer(AtomicDEVS):
         priority=PRIORITIZE_BIGGER_SHIPS,
     ):
         super().__init__("FillErUpLoadBalancer")
-        # self.state = LoadBalancerState(...)
+        self.state = LoadBalancerState(lock_capacities=lock_capacities, priority=priority)
 
     # def extTransition(self, inputs):
     #     pass
@@ -65,6 +86,12 @@ class FillErUpLoadBalancer(AtomicDEVS):
     
     # def intTransition(self):
     #     pass
+    
+class LockState:
+    remaining_capacity: int
+    
+    def __init__(self, capacity):
+        self.remaining_capacity = capacity
 
 class Lock(AtomicDEVS):
     def __init__(self,
@@ -73,7 +100,9 @@ class Lock(AtomicDEVS):
         passthrough_duration=60.0*15.0, # how long does it take for the lock to let a ship pass through it
     ):
         super().__init__("Lock")
-        # self.state = LockState(...)
+        self.state = LockState(capacity=capacity)
+        self.max_wait_duration = max_wait_duration
+        self.passthrough_duration = passthrough_duration
 
     # def extTransition(self, inputs):
     #     pass
