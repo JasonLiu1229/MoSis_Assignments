923e075562c01f81f6299d1b850d0ed728b04dab
Kin Ning Shum <kinningshum@hotmail.com>
2024-12-19 22:03:01 +0100

    Modified queue, added superclass to loadbalancers

--- Assignment_5/assignment/atomicdevs.py
+++ Assignment_5/assignment/atomicdevs.py
@@ -5,16 +5,21 @@ from pypdevs.DEVS import AtomicDEVS
 from environment import *
 import random
 import dataclasses
-import queue
+
 
 @dataclasses.dataclass
 class QueueState:
-    queues: dict[int, queue.Queue]
-    waiting: bool
+    queues: dict[int, list]
+    request: bool
+    pop_index: int
+    new_size_available: bool
     
     def __init__(self, ship_sizes):
-        self.queues = {size: queue.Queue() for size in ship_sizes}
-        self.waiting = False
+        self.queues = {size: [] for size in ship_sizes}
+        self.request = False
+        self.pop_index = 0
+        self.new_size_available = False
+
 
 class Queue(AtomicDEVS):
     def __init__(self, ship_sizes):
@@ -24,36 +29,55 @@ class Queue(AtomicDEVS):
         self.state = QueueState(ship_sizes=ship_sizes)
         
         self.in_ship = self.addInPort("in_queue")
+        self.in_request = self.addInPort("in_request")
+
         self.out_ship = self.addOutPort("out_queue")
+        self.out_available = self.addOutPort("out_available")
 
     def extTransition(self, inputs):
+        # ship arrival
         if self.in_ship in inputs:
             ship = inputs[self.in_ship]
-            self.state.queues[ship.size].put(ship)
-            self.state.waiting = True
+            if not self.state.queues[ship.size]:
+                self.state.new_size_available = True
+            self.state.queues[ship.size].append(ship)
+
+        # ship request from loadbalancer
+        if self.in_request in inputs:
+            self.state.request = True
+            self.state.pop_index = inputs[self.in_request]
+
         return self.state
     
     def timeAdvance(self):
-        return 0 if self.state.waiting else float("inf")
+        return 0 if self.state.request or self.state.new_size_available else float("inf")
     
     def outputFnc(self):
-        for size, queue in self.state.queues.items():
-            if not queue.empty():
-                return {self.out_ship: queue.queue[0]}
-        return {}
+        output = {}
+        # notify new ship availabilities
+        if self.state.new_size_available:
+            # output all ship sizes with non-empty queue
+            output[self.out_available] = [i for i, q in self.state.queues.items() if not q]
+
+        # output ship
+        if self.state.request:
+            output[self.out_ship] = self.state.queues[self.state.pop_index][0]
+        
+        return output
     
     def intTransition(self):
-        self.state.waiting = any(not q.empty() for q in self.state.queues.values())
-        # pop the first ship from the queue
-        for size, queue in self.state.queues.items():
-            if not queue.empty():
-                queue.get()
-                break
-        self.state.waiting = any(not q.empty() for q in self.state.queues.values())
+        self.state.new_size_available = False
+
+        if self.state.request:
+            self.state.queues[self.state.pop_index].pop(0)
+            self.state.request = False
+
         return self.state
+
 
 PRIORITIZE_BIGGER_SHIPS = 0
 PRIORITIZE_SMALLER_SHIPS = 1
+
 
 @dataclasses.dataclass
 class LoadBalancerState:
@@ -66,18 +90,29 @@ class LoadBalancerState:
         self.current_lock = 0
         self.priority = priority
 
-class RoundRobinLoadBalancer(AtomicDEVS):
+
+class LoadBalancer(AtomicDEVS):
+    def __init__(self,
+        name="LoadBalancer",
+        lock_capacities=[3,2], # two locks of capacities 3 and 2.
+        priority=PRIORITIZE_BIGGER_SHIPS,
+    ):
+        super().__init__(name)
+        self.state = LoadBalancerState(lock_capacities=lock_capacities, priority=priority)
+        
+        self.in_ship = self.addInPort("in_event") # ship inbound
+        self.in_ships_available = self.addInPort("in_available") # which sizes of ships are in the queue
+        
+        self.out_request = self.addOutPort("out_request_ship") # send a request for a ship of a size
+        self.out_locks = [self.addOutPort(f"out_event_to_lock{i}" for i in range(len(lock_capacities)))] # list of all locks
+
+
+class RoundRobinLoadBalancer(LoadBalancer):
     def __init__(self,
         lock_capacities=[3,2], # two locks of capacities 3 and 2.
         priority=PRIORITIZE_BIGGER_SHIPS,
     ):
-        super().__init__("RoundRobinLoadBalancer")
-        self.state = LoadBalancerState(lock_capacities=lock_capacities, priority=priority)
-        
-        self.in_ship = self.addInPort("in_event")
-        
-        self.out_lock1 = self.addOutPort("out_event_to_lock1")
-        self.out_lock2 = self.addOutPort("out_event_to_lock2")
+        super().__init__("RoundRobinLoadBalancer", lock_capacities, priority)
 
     def extTransition(self, inputs):
         # sort based on ship size
@@ -114,18 +149,14 @@ class RoundRobinLoadBalancer(AtomicDEVS):
                 self.state.lock_capacities[i] = (lock_capacity, lock_ships)
         return self.state
 
-class FillErUpLoadBalancer(AtomicDEVS):
+
+class FillErUpLoadBalancer(LoadBalancer):
     def __init__(self,
         lock_capacities=[3,2], # two locks of capacities 3 and 2.
         priority=PRIORITIZE_BIGGER_SHIPS,
     ):
-        super().__init__("FillErUpLoadBalancer")
-        self.state = LoadBalancerState(lock_capacities=lock_capacities, priority=priority)
-        
-        self.in_ship = self.addInPort("in_event")
-        
-        self.out_lock1 = self.addOutPort("out_event_to_lock1")
-        self.out_lock2 = self.addOutPort("out_event_to_lock2")
+        super().__init__("FillErUpLoadBalancer", lock_capacities, priority)
+
 
     def extTransition(self, inputs):
         if self.in_ship in inputs:
